ecbe164d790ddfd6ff0284958235b69af12ad3ff
Chris Hermut <chris.hermut@hotmail.com>
2016-05-30 20:19:02 +0200

    Basic material UI

--- Azimo/Scripts/app.js
+++ Azimo/Scripts/app.js
@@ -3,6 +3,11 @@
         $mdThemingProvider.theme('altTheme')
           .primaryPalette('purple')
     })
+    .service('appStateSrvc', function () {
+        return {
+            loggedIn: false
+        }
+    })
     .service('helpersSrvc', function ($http, $q) {
         return {
             login: function () {
@@ -19,16 +24,44 @@
             }
         }
     })
-    .controller('indexCtrl', function ($scope, helpersSrvc) {
+    .controller('indexCtrl', function ($scope, appStateSrvc, helpersSrvc) {
         // Initial
         $scope.userData = {};
+        $scope.tooltip = {
+            visible: false,
+            message: ''
+        };
+
+        $scope.appState = appStateSrvc;
 
         $scope.logIn = function () {
             helpersSrvc.login()
               .then(function (result) {
                   $scope.userData = result;
+                  appStateSrvc.loggedIn = true;
+
+                  console.log($scope.userData.data.Login);
               }, function (result) {
                   // TODO: Indicate error
               })
+        };
+
+        $scope.logOut = function () {
+            appStateSrvc.loggedIn = false;
+            $scope.userData = {};
+        };
+
+        $scope.showTooltip = function (target) {
+            $scope.tooltip.visible = true;
+
+            if (target == 'SA') {
+                $scope.tooltip.message = 'Basic authentication - it will not persist state';
+            } else if (target == 'OA') {
+                $scope.tooltip.message = 'OAUTH authentication - persists state after reload';
+            }
+        };
+
+        $scope.hideTooltip = function () {
+            $scope.tooltip.visible = false;
         }
     })
