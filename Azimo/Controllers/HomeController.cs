7c9cc26198891f34d5901408661447e488da0854
Chris Hermut <chris.hermut@hotmail.com>
2016-05-30 19:27:10 +0200

    Angular material added

--- Azimo/Controllers/HomeController.cs
+++ Azimo/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Web.Mvc;
+using Octokit;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Azimo.Controllers
 {
@@ -7,6 +10,18 @@ namespace Azimo.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<string> Login()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("Azimo"));
+
+            var basicAuth = new Credentials("chris.hermut@gmail.com", "werty1234");
+            client.Credentials = basicAuth;
+
+            User user = await client.User.Current();
+
+            return JsonConvert.SerializeObject(user);
         }
     }
 }
--- Azimo/Scripts/app.js
+++ Azimo/Scripts/app.js
@@ -1,1 +1,34 @@
-﻿angular.module('app', [])
+﻿angular.module('app', ['ngMaterial'])
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('altTheme')
+          .primaryPalette('purple')
+    })
+    .service('helpersSrvc', function ($http, $q) {
+        return {
+            login: function () {
+                var q = $q.defer();
+
+                $http({ method: 'GET', url: 'Home/Login' })
+                .then(function (result) {
+                    q.resolve(result);
+                }, function () {
+                    q.resolve([]);
+                })
+
+                return q.promise
+            }
+        }
+    })
+    .controller('indexCtrl', function ($scope, helpersSrvc) {
+        // Initial
+        $scope.userData = {};
+
+        $scope.logIn = function () {
+            helpersSrvc.login()
+              .then(function (result) {
+                  $scope.userData = result;
+              }, function (result) {
+                  // TODO: Indicate error
+              })
+        }
+    })
